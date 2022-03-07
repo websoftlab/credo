b0e2cd575a86fcdf55a6ceeedaf79d1818a3dea9
Joe Blanket <credo@websoftlab.com>
2022-03-07 03:31:22 +0300

    Base files added

---
+++ types/global.d.ts
@@ -0,0 +1,53 @@
+import type {CredoJS, Route, Worker as WorkerFJS} from "@credo-js/server";
+import type {AxiosRequestConfig} from "axios";
+import type {Lexicon} from "@credo-js/lexicon";
+import type {URL} from "@credo-js/make-url";
+
+// for all
+
+declare global {
+	export var __DEV__: boolean;
+	export var __DEV_SERVER__: boolean;
+	export var __PROD__: boolean;
+	export var __BUNDLE__: string;
+	export var __SSR__: boolean;
+	export var credo: CredoJS;
+}
+
+// @credo-js/responder-page
+
+declare module "axios" {
+	export interface AxiosRequestConfig {
+		isPage?: boolean;
+	}
+}
+
+// @credo-js/server
+
+declare module 'cluster' {
+	interface Worker {
+		workerData?: WorkerFJS.Data;
+	}
+}
+
+declare module "koa" {
+	interface Context {
+		readonly credo: CredoJS;
+
+		// lexicon extension
+		readonly multilingual: boolean;
+		readonly defaultLanguage: string;
+		readonly languages: string[];
+		language: string;
+
+		// url
+		makeUrl: URL.AsyncHandler;
+
+		// app
+		readonly store: Lexicon.StoreInterface;
+
+		// router
+		match?: Record<string, any>;
+		route?: Route.Context;
+	}
+}
