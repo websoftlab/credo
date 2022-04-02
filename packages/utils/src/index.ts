042335a2054ab43e331d807357c4f1cb8cd7cfff
Joe Blanket <credo@websoftlab.com>
2022-04-02 16:19:09 +0300

    Removed deprecated function from @credo-js/utils package

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,8 +1,5 @@
 export {default as asyncResult} from "./asyncResult";
 export {default as isAsync} from "./isAsync";
-export {default as objectDefineProperty} from "./objectDefineProperty";
 export {default as callIn} from "./callIn";
 export {default as clonePlainObject} from "./clonePlainObject";
 export {default as htmlEscape} from "./htmlEscape";
-
-export type {ObjectDescriptor} from "./objectDefineProperty";
